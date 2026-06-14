17f0d6eccce0be3698306f735d89eeebc7613f8a
Amos-developer <amosnyoni186@gmail.com>
2026-06-14 23:02:02 +0300

    Innitial configurations

--- src/server.js
+++ src/server.js
@@ -1,15 +1,7 @@
-import express from 'express';
+import app from "./app";
 
 const PORT = process.env.PORT || 3000;
-const app = express();
-app.use(express.json());
 
-app.get("/", (req, res) =>{
-    res.json({
-        message: "Vikoba API is running"
-    });
-});
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
