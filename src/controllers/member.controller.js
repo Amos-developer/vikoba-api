2a3e6ea6e0a243a30703d10d77d1ceab12e28514
Amos-developer <amosnyoni186@gmail.com>
2026-06-15 03:25:43 +0300

    Update the controller

--- src/controllers/member.controller.js
+++ src/controllers/member.controller.js
@@ -1,70 +1,48 @@
 import { Member } from "../models/member.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-// Create a new member
+// Create member
 export const createMember =
-  async (req, res) => {
-    try {
-      const member =
-        await Member.create(req.body);
+  asyncHandler(async (req, res) => {
+    const member =
+      await Member.create(req.body);
 
-      res.status(201).json({
-        success: true,
-        data: member,
-      });
-    } catch (error) {
-      res.status(500).json({
-        success: false,
-        message: error.message,
-      });
-    }
-  };
+    res.status(201).json({
+      success: true,
+      data: member,
+    });
+  });
 
-//   Get all members
+// Get all members
 export const getMembers =
-  async (req, res) => {
-    try {
-      const members =
-        await Member.findAll();
+  asyncHandler(async (req, res) => {
+    const members =
+      await Member.findAll();
 
-      res.json({
-        success: true,
-        data: members,
-      });
-    } catch (error) {
-      res.status(500).json({
-        success: false,
-        message: error.message,
-      });
-    }
-  };
+    res.status(200).json({
+      success: true,
+      data: members,
+    });
+  });
 
-//   Get a member by ID
+// Get member by ID
 export const getMemberById =
-  async (req, res) => {
-    try {
-      const member =
-        await Member.findById(
-          req.params.id
-        );
+  asyncHandler(async (req, res) => {
+    const member =
+      await Member.findById(
+        req.params.id
+      );
 
-      if (!member) {
-        return res
-          .status(404)
-          .json({
-            success: false,
-            message:
-              "Member not found",
-          });
-      }
-
-      res.json({
-        success: true,
-        data: member,
-      });
-    } catch (error) {
-      res.status(500).json({
-        success: false,
-        message: error.message,
-      });
+    if (!member) {
+      const error = new Error(
+        "Member not found"
+      );
+      error.statusCode = 404;
+      throw error;
     }
-  };
+
+    res.status(200).json({
+      success: true,
+      data: member,
+    });
+  });
